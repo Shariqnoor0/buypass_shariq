9271c7735f0e2179f3c6a04a1aafcbb7d103b3a2
Shariqnoor0 <shariq.noor@virtuosoft.pk>
2025-11-19 18:08:22 +0500

    Delete all SQL files from user folder

--- user/v_category_with_prod_l1_l9_final.sql
+++
@@ -1,9 +0,0 @@
- SELECT products._id AS product_id,
-    products.nameen AS product_name,
-    products.seller_id,
-    products.seller_name,
-    products.seller_city,
-    category_info.value AS category_name,
-    concat('L', category_info.ordinality) AS category_level
-   FROM stg.products,
-    LATERAL jsonb_array_elements_text((products.category_cateogrypath)::jsonb) WITH ORDINALITY category_info(value, ordinality);
--- user/vw_products_subproducts.sql
+++
@@ -1,7 +0,0 @@
- SELECT p.nameen,
-    p.category_name,
-    sp.sku,
-    sp.price,
-    sp.stock
-   FROM (stg.subproducts sp
-     JOIN stg.products p ON ((p._id = sp.productid)));
